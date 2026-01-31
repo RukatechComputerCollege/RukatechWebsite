fe61491340590eb83a746607429fdd0818aad266
EonSyntax <eonsyntax@gmail.com>
2026-01-31 15:59:05 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from './pages/Home.jsx'
+import { Route, Routes } from 'react-router'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import LandingPage from './pages/Landingpage.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} >
+          <Route index element={<LandingPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+
+  const navLinks = [
+    { name: "HOME", href: "#", active: true },
+    { name: "ABOUT", href: "/about", hasDropdown: true },
+    { name: "TRAINING", href: "#", hasDropdown: true },
+    { name: "STUDENT PORTAL", href: "#" },
+    { name: "CONTACT US", href: "/contact" },
+  ];
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Navbar
